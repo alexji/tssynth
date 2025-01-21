ef4f9b5bd8948304ad5e508fc24f12b3df36b8ae
Alexander Ji <alexji@uchicago.edu>
2025-01-20 21:56:26 -0600

    download model atmospheres

--- src/tssynth/downloader.py
+++ src/tssynth/downloader.py
@@ -3,6 +3,8 @@ from tqdm import tqdm
 import os
 import yaml
 from importlib.resources import files as resource_files
+import zipfile
+import time
 
 TSDEPCOEFF_PATH = os.environ.get('TSDEPCOEFF_PATH', None)
 ALLMARCS_PATH = os.environ.get('ALLMARCS_PATH', None)
@@ -35,6 +37,13 @@ def get_nlte_depgrid_info():
         nlte_info = yaml.load(fp, Loader=yaml.FullLoader)
     return nlte_info
 
+def get_marcs_model_list():
+    ## TODO: this is not recommended, but let's keep it for now...
+    marcs_info_path = resource_files('tssynth').joinpath('../../data/model_list.txt')
+    with open(marcs_info_path, "r") as fp:
+        lines = [x.strip() for x in fp.readlines()]
+    return lines
+
 def download_nlte_depgrid(element):
     """
     Downloads the NLTE departure coefficients for the given element from the MPIA Bergemann Group
@@ -60,3 +69,23 @@ def download_nlte_depgrid(element):
         print(f"Downloading {file} from {url} to {local_path} (May be very large!)")
         download_file(url, local_path)
         print(f"Downloaded {file} to {local_path}")
+
+def download_model_atmospheres():
+    url = "https://keeper.mpdl.mpg.de/d/6eaecbf95b88448f98a4/files/?p=%2Fatmospheres%2Fmarcs_standard_comp.zip&dl=1"
+    model_list = get_marcs_model_list()
+    missing_files = [model for model in model_list if not os.path.exists(os.path.join(ALLMARCS_PATH, model))]
+    if not missing_files:
+        print(f"All files from model_list.txt are present in {ALLMARCS_PATH}.")
+        print("No need to download files.")
+        return
+    print(f"{ALLMARCS_PATH} is missing {len(missing_files)} model atmospheres out of {len(model_list)}")
+    print(f"Downloading the entire model atmosphere archive from {url} to {ALLMARCS_PATH}")
+    
+    local_path = os.path.join(ALLMARCS_PATH, "marcs_standard_comp.zip")
+    download_file(url, local_path)
+    print(f"Downloaded model atmospheres to {local_path}, now unzipping...")
+    start_time = time.time()
+    with zipfile.ZipFile(local_path, 'r') as zip_ref:
+        zip_ref.extractall(ALLMARCS_PATH)
+    end_time = time.time()
+    print(f"Unzipping completed in {end_time - start_time:.2f} seconds.")
--- tests/test_download.py
+++ tests/test_download.py
@@ -1,9 +1,21 @@
 import tssynth.downloader
 
+def test_get_nlte_depgrid_info():
+    nlte_info = tssynth.downloader.get_nlte_depgrid_info()
+    assert "H" in nlte_info
+
+def test_get_marcs_model_list():
+    marcs_models = tssynth.downloader.get_marcs_model_list()
+    # assert len(marcs_models) == 
+
 def test_download_nlte_depgrid():
     ## TODO not totally sure what to do here as a test without actually just downloading everything
     # I tested it on my computer and it works though...
     # tssynth.downloader.download_nlte_depgrid("H")
     nlte_info = tssynth.downloader.get_nlte_depgrid_info()
     assert "H" in nlte_info
-    pass
+
+def test_download_model_atmospheres():
+    ## TODO not totally sure what to do here as a test without actually just downloading everything
+    # I tested it on my computer and it works though...
+    tssynth.downloader.download_model_atmospheres()
