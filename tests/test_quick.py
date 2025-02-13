13f32ace8d64a037d8a6e04534e3bc9ddf908dc8
Alex Ji <alexji@uchicago.edu>
2025-02-13 15:28:26 -0600

    Update default linelist

--- tests/test_quick.py
+++ tests/test_quick.py
@@ -18,18 +18,14 @@ def test_simple_methods():
     assert MH == -2.0
     assert aFe == 0.4
     assert spherical == True
-    # linelistnames = synthesizer.get_default_linelist_filenames()
-    # assert len(linelistnames) == 15
-    # linelistnames = synthesizer.get_default_linelist_filenames(include_H=False)
-    # assert len(linelistnames) == 14
-    # linelistnames = synthesizer.get_vald_linelist_filenames()
-    # assert len(linelistnames) == 14
-    # linelistnames = synthesizer.get_vald_linelist_filenames(include_H=False)
-    # assert len(linelistnames) == 13
     linelistnames = synthesizer.get_default_linelist_filenames()
-    assert len(linelistnames) == 6
+    assert len(linelistnames) == 8
+    for fname in linelistnames:
+        assert os.path.exists(fname), fname
     linelistnames = synthesizer.get_default_linelist_filenames(include_H=False)
-    assert len(linelistnames) == 5
+    assert len(linelistnames) == 7
+    for fname in linelistnames:
+        assert os.path.exists(fname), fname
 
 def test_run_synth_lte_quick_1():
     wmin, wmax, dw = 5090, 5100, 0.05
